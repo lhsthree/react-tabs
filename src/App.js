85e150cd8355d445ab61283ed5cadb7200b4568a
lhsthree <lhsthree@gmail.com>
2021-02-03 12:32:45 -0600

    feat: added tabs to cahnge index value and job

--- src/App.js
+++ src/App.js
@@ -29,34 +29,47 @@ return <section className="section loading">
 
 const { company, dates, duties, title} = jobs[value]
 
-  return <section className="section">
-    <div className="title">
-      <h2>experience</h2>
-      <div className="underline"></div>
-      <div className="jobs-center">
-        {/*button container */ }
-        <div className="btn-container"></div>
-      {
-        jobs.map((item,index) => {
-          return <button key={item.id} onClick={() => setValue(index)}>
-            {item.company}
-          </button>
-        })
-      }
-      <article className="job-info">
-        <h3>{title}</h3>
-        <h4>{company}</h4>
-        <p className="job-date">{dates}</p>
-        {duties.map((duty, index) => {
-          return <div key={index} className="job-desc"><FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
-          <p>{duty}</p></div>
-        })}
-      </article>
+    return (
+    <section className='section'>
+      <div className='title'>
+        <h2>expierence</h2>
+        <div className='underline'></div>
       </div>
-    </div>
-  </section>
-
-
+      <div className='jobs-center'>
+        {/* btn container */}
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <button type='button' className='btn'>
+        more info
+      </button>
+    </section>
+  )
 }
 
 export default App
